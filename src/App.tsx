1ad206d4e10c45441540ef29f356e97ed003da39
David Vail <dcvail17@gmail.com>
2026-02-08 16:28:13 -0500

    task(004): add C major scale example

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import psychoKiller from './lib/examples/talking-heads---psycho-killer.json';
+import majorScale from './lib/examples/major-scale-c.json';
 import type { Track } from './lib/model';
 
 import { Legend } from './components/Legend';
@@ -6,12 +7,14 @@ import { TrackView } from './components/TrackView';
 
 function App() {
   const track = psychoKiller as Track;
+  const scaleTrack = majorScale as Track;
 
   return (
     <div data-theme="cupcake" className="min-h-screen">
       <div className="container mx-auto flex flex-col gap-8 px-6 py-12">
         <Legend />
         <TrackView track={track} />
+        <TrackView track={scaleTrack} />
       </div>
     </div>
   );
