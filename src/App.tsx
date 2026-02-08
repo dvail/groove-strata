120ca4a3cde7873f87529be24712678785a28d6e
David Vail <dcvail17@gmail.com>
2026-02-08 16:53:13 -0500

    task(004): add A minor scale example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import psychoKiller from './lib/examples/talking-heads---psycho-killer.json';
 import majorScale from './lib/examples/major-scale-c.json';
+import minorScale from './lib/examples/minor-scale-a.json';
 import type { Track } from './lib/model';
 
 import { Legend } from './components/Legend';
@@ -8,6 +9,7 @@ import { TrackView } from './components/TrackView';
 function App() {
   const track = psychoKiller as Track;
   const scaleTrack = majorScale as Track;
+  const minorScaleTrack = minorScale as Track;
 
   return (
     <div data-theme="cupcake" className="min-h-screen">
@@ -15,6 +17,7 @@ function App() {
         <Legend />
         <TrackView track={track} />
         <TrackView track={scaleTrack} />
+        <TrackView track={minorScaleTrack} />
       </div>
     </div>
   );
